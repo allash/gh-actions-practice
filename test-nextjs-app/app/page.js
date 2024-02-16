bbc046d774120c4ce580903f94500530feaa8082
allash <1172129+allash@users.noreply.github.com>
2024-02-16 16:24:33 +0100

    added test nextjs app

---
+++ test-nextjs-app/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div class="flex items-center justify-center h-screen">
+        <h1 class="text-2xl font-bold">Hello World</h1>
+      </div>
+    </main>
+  );
+}
