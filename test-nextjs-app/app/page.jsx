bf73e1ae0fe3137ee79e574e0285105645c96c92
allash <1172129+allash@users.noreply.github.com>
2024-02-19 15:50:28 +0100

    fix

--- test-nextjs-app/app/page.jsx
+++ test-nextjs-app/app/page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div class="flex items-center justify-center h-screen">
-        <h1 class="text-2xl font-bold">Hello World</h1>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">Hello World</h1>
       </div>
     </main>
   );
